7888088f0892684ba0399f3110839373ec8b8ecb
ullas9080 <ullas9080@gmail.com>
2025-12-02 01:25:02 +0530

    completed finally

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true })); // for form data
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/user/", UserRoutes);
 
@@ -43,7 +43,7 @@ const connectDB = () => {
 const startServer = async () => {
   try {
     connectDB();
-    app.listen(8080, () => console.log("Server started on port 8080"));
+    // app.listen(8080, () => console.log("Server started on port 8080"));
   } catch (error) {
     console.log(error);
   }
